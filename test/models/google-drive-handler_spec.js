28f36267ba832a59c59b5ebec68eaa2bb204f862
gingin77 <ginniehench@gmail.com>
2019-12-03 20:49:20 -0500

    Add spec for google-drive-handler

---
+++ test/models/google-drive-handler_spec.js
@@ -0,0 +1,44 @@
+const chai = require("chai"),
+  expect = chai.expect,
+  gds = require("../../client"),
+  driveService = new gds.GoogleDriveService(),
+  drive = driveService.drive(),
+  gdh = require("../../google-drive-handler"),
+  GoogleDriveHandler = gdh.GoogleDriveHandler,
+  { i, o } = require("../fixtures/google-drive-handler.js");
+
+function getNewGoogleDriveHandler(inputArguments, drive) {
+  return new GoogleDriveHandler(
+      inputArguments,
+      drive
+  );
+}
+
+describe("GoogleDriveHandler", () => {
+  describe("results returned", ()=> {
+    context("when directory query with depth of one", () => {
+      it("should return direct child contents, which are usually also folders", async () => {
+        let googleDriveHandler = getNewGoogleDriveHandler(
+          i.singleDirectoryDepthOne,
+          drive
+        );
+        const data = await googleDriveHandler.queryHandler();
+
+        expect(data.result).to.eql(o.singleDirectoryDepthOne);
+      });
+    });
+
+    context("when directory query with depth of two", () => {
+      it("should return child contents of subdirectory, which are usually media files", async () => {
+        let googleDriveHandler = getNewGoogleDriveHandler(
+          i.singleDirectoryDepthTwo,
+          drive
+        );
+        const data = await googleDriveHandler.queryHandler();
+
+        expect(data.result).to.eql(o.singleDirectoryDepthTwo);
+      });
+    })
+
+  })
+});
