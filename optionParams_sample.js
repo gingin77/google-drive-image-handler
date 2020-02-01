8664cb7e160733617e59ac6fec1927050d221167
gingin77 <ginniehench@gmail.com>
2020-01-31 19:40:14 -0500

    Remove outdated optionParams_sample

--- optionParams_sample.js
+++
@@ -1,8 +0,0 @@
-let optionParams = {
-  pageSize: 4,
-  q:
-    '"alphanumeric-string-for-a-dir-on-google-drive" in parents or "alphanumeric-string-for-a-dir-on-google-drive" in parents',
-  fields: "nextPageToken, files(id, name, mimeType)"
-};
-
-module.exports = optionParams;
